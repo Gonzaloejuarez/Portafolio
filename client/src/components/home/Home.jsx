5884b4b281eb84cbf5241dcceae6919acad4b157
Gonzalo Juarez <Juarezgonzalo000@gmail.com>
2022-08-04 20:56:38 -0300

    Subiendo carpetas

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export const Home = () => {
+
+	return(
+		<div> 
+			<h1>Bienvenidos</h1>
+			<section id="#inicio">
+				<div> 
+					<h1>Gonzalo Juarez Developer</h1>
+				</div>
+			</section>
+		</div>
+	)
+}
+
+export default Home
